070d2aa8d5fcb2adb98e85e137ca1830cf9b3574
digitechgrouperu <digitechgrouperu@gmail.com>
2024-12-19 09:42:20 -0500

    upd api migrate route

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/migrate', function () {
+    \Artisan::call('migrate');
+    dd('migrated!');
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
